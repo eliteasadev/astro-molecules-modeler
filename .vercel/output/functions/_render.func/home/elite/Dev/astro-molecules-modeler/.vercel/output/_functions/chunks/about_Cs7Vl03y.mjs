fac5eab6615455338588b28973f65a5299d42718
eliteasdev <elitefirelight.dev@gmail.com>
2024-06-07 11:08:43 -0600

    Vercel fix deploy a1

--- .vercel/output/functions/_render.func/home/elite/Dev/astro-molecules-modeler/.vercel/output/_functions/chunks/about_Cs7Vl03y.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/about_DO6ZGBL1.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/home/elite/Dev/astro-molecules-modeler/.vercel/output/_functions/chunks/generic_B49MIqC5.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_xHquv5rD.mjs');
-
-export { page };
